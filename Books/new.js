a6c1a97893d0e8ab9aa365bed2e6c1c51a997cbe
Anusuya Xavier <anusuyat43@gmail.com>
2024-07-08 12:34:18 +0530

    add

---
+++ Books/new.js
@@ -0,0 +1,13 @@
+ 
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString("Happy,Day"));
+
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
+
+    console.log(reverseString("Happy,Day."));
