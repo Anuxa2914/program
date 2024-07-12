04dff0bb1c8f6655b97307314e49d7cc89916cd3
Anusuya Xavier <anusuyat43@gmail.com>
2024-07-12 15:09:43 +0530

    header created

--- Ecommerce/ecommerce.js
+++ Ecommerce/ecommerce.js
@@ -1,13 +1,13 @@
-var sidenavbar=document.querySelector(".side-navbar")
+var sidenav=document.querySelector(".side-navbar")
 
 
 function shownavbar()
 {
-
+  sidenav.style.left="0%"
 }
 
 
 function closenavbar()
 {
-    
+   sidenav.style.left="-60%"
 }
